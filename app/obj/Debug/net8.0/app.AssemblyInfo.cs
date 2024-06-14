de15e373a083c521ff0112fbd3fe5c319a0d0622
Thabo Miles-Matli <164856759+ProfMiles@users.noreply.github.com>
2024-06-14 08:02:04 +0000

    Code migrated from AQA file into program.cs. Interface is now loaded and we can play. The original AQA file was deleted because the namespaces were clashing.

--- app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7144bb87078822509301df84d33d05254c4d1bee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f320b6be52b3e3d7f78354267ad913bfb83ae16d")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
